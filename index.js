0ddaa56b8651a3c92f5832236bf4d28886b3e4bc
htoo-aung <113065976+htoo-aung@users.noreply.github.com>
2025-01-08 17:23:03 -0600

    yeet

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const myLibrary = [];
+document.addEventListener("DOMContentLoaded", () => {
+  displayBooks(myLibrary);
+});
+
+const book1 = new Book("hello", "h", 12);
+const book2 = new Book("hello", "h", 12);
+const book3 = new Book("hello", "h", 12);
+
+const myLibrary = [book1, book2, book3];
 
 function Book(title, author, pages) {
   this.title = title;
@@ -8,4 +16,17 @@ function Book(title, author, pages) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+}
+
+function displayBooks(arr) {
+  const mainContainer = document.getElementById("main");
+
+  arr.forEach((book) => {
+    const newCard = document.createElement("div");
+    newCard.classList.add("card");
+
+    // create and add book properties to the card text display
+
+    mainContainer.appendChild(newCard);
+  });
 }
