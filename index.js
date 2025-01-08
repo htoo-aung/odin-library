d0e665906f819ee4cf0727f1980fa1bb3a7aab4c
htoo-aung <113065976+htoo-aung@users.noreply.github.com>
2025-01-08 11:28:15 -0600

    project files init

---
+++ index.js
@@ -0,0 +1,11 @@
+const myLibrary = [];
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
