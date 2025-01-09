5710ce8374dc87635c1dd16159b32d8025a38481
htoo-aung <113065976+htoo-aung@users.noreply.github.com>
2025-01-09 09:52:22 -0600

    added modal THING TOOK ME FOREVER

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   displayBooks(myLibrary);
+  addBook();
+  closeModal();
 });
 
-const book1 = new Book("hello", "h", 12);
-const book2 = new Book("hello", "h", 12);
-const book3 = new Book("hello", "h", 12);
-
-const myLibrary = [book1, book2, book3];
+const myLibrary = [];
 
 function Book(title, author, pages) {
   this.title = title;
@@ -26,7 +24,43 @@ function displayBooks(arr) {
     newCard.classList.add("card");
 
     // create and add book properties to the card text display
+    newCard.innerHTML = 
+    `
+      <h1 class="book-title">${book.title}</h1>
+      <p class="book-author">author. ${book.author}</p>
+      <p class="book-pages">pages. ${book.pages}</p>
+      <div class="book-status">
+        <label for="read" >read.</label>
+        <input class="book-read" type="checkbox" id="read" name="read">
+      </div>
+    `;
 
+    // add card to the main container
     mainContainer.appendChild(newCard);
+  });
+}
+
+function readBook() {
+
+}
+
+function addBook() {
+  const addBtn = document.getElementById("btn-add");
+  const modal = document.getElementById("modal");
+  console.log("clicked");
+  
+  addBtn.addEventListener("click", () => {
+    modal.showModal();
+    modal.classList.add("modal-show");
+  });
+}
+
+function closeModal() {
+  const modal = document.getElementById("modal");
+  const modalExitBtn = document.getElementById("close-modal-btn");
+
+  modalExitBtn.addEventListener("click", () => {
+    modal.close();
+    modal.classList.remove("modal-show");
   });
 }
