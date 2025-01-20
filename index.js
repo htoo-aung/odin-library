cffda705e06381dc873d8a01a9c751ed1bf812b0
htoo-aung <113065976+htoo-aung@users.noreply.github.com>
2025-01-20 15:07:59 -0600

    implemented createCard function

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const myLibrary = [];
 class Book {
   /**
    * Creates a new Book instance.
-   * @param {string} title - Title of the book
-   * @param {string} author - Author of the book
-   * @param {number} pages - The number of pages in the book
-   * @param {boolean} read - The read status of the book
+   * @param {String} title - Title of the book
+   * @param {String} author - Author of the book
+   * @param {Number} pages - The number of pages in the book
+   * @param {Boolean} read - The read status of the book
    */
   constructor(title, author, pages) {
     this.title = title;
@@ -34,13 +34,28 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-function createCard() {
-  // create a div make a card and add the proper elements to them
-  // div class card > title, author, page, read button
+/**
+ * Creates a card container to hold all the data of the Book object in p elements.
+ * @param {String} title - Title of the Book object
+ * @param {String} author - Author of the Book object
+ * @param {Number} pages - Author of the Book object
+ * @returns A div with elements holding book data as a children; a card
+ */
+function createCard(title, author, pages) {
   const newCard = document.createElement('div');
-  const cardTitle = document.createElement('p');
-  const cardAuthor = document.createElement('p');
-  const cardPages = document.createElement('p');
-  const readButton = document.createElement('button');
+  newCard.classList.add('card');
 
+  newCard.innerHTML = 
+  `
+    <p class="book-title">${title}</p>
+    <p class="book-author">${author}</p>
+    <p class="book-pages">${pages}</p>
+    <button class="book-read-btn">Read</button>
+  `;
+
+  return newCard;
 }
+
+const test = document.getElementById('main');
+const card = createCard("hello", "htoo", 25);
+test.appendChild(card);
